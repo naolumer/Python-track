265290c66bfad68d5334ea802aa9e220d26a1f52
naolumer <naolumer3@gmail.com>
2024-05-12 22:30:41 +0300

    additional changes

--- cp/DAY-20/main.py
+++ cp/DAY-20/main.py
@@ -1,12 +1,20 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
+
+
 screen= Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My snake Game")
 screen.tracer(0)
+
 snake= Snake()
+food= Food()
+scoreboard= Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
@@ -20,16 +28,21 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    
 
+    if snake.head.distance(food)<15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
         
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() < -290 or snake.head.ycor() > 290:
+        is_game_on= False
+        scoreboard.game_over()
+    
+    for segment in snake.segment[1:]:
+            if snake.head.distance(segment)<10:
+                is_game_on= False
+                scoreboard.game_over()
             
-
-
-
-
-
-
 
 
 screen.exitonclick()
--- cp/DAY-20/snake.py
+++ cp/DAY-20/snake.py
@@ -5,6 +5,7 @@ UP= 90
 DOWN=270
 LEFT= 180
 RIGHT=0
+
 class Snake:
     def __init__(self):
         self.segment=[]
@@ -13,16 +14,26 @@ class Snake:
         
     def create_snake(self):
         for position in STARTING_POSITION:
-            new_segment= Turtle(shape= "square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segment.append(new_segment)
+            self.add_segment(position)
+
+            
+    def add_segment(self,position):
+        new_segment= Turtle(shape= "square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segment.append(new_segment)
+    
+        
+    def extend(self):
+        self.add_segment(self.segment[-1].position())
+        
     
     def move(self):
         for seg_num in range(len(self.segment)-1, 0, -1):
             new_x= self.segment[seg_num-1].xcor()
             new_y= self.segment[seg_num-1].ycor()
+            
             self.segment[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
         
