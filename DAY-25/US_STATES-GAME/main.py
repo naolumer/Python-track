8d8292b872d231878c1274a32c391ed9284f948f
naolumer <naolumer3@gmail.com>
2024-06-25 22:36:09 +0300

    Day-25

---
+++ DAY-25/US_STATES-GAME/main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+screen = turtle.Screen()
+screen.title("US-states-game")
+image="./DAY-25/US_STATES-GAME/blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# answer_state= screen.textinput(title="Guess The State", prompt="What's another state? ")
+
+# print(answer_state)
+
+data=pandas.read_csv("./DAY-25/US_STATES-GAME/50_states.csv")
+all_states=data.state.to_list()
+guessed_states=[]
+
+while len(guessed_states)<50:
+    
+    answer_state=screen.textinput(title=f"{len(guessed_states)}/50 states Correct",
+    prompt="What's another state's namae").title()
+    
+    if answer_state== "Exit":
+        missing_states=[]
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data= pandas.DataFrame(missing_states)
+        new_data.to_csv("./DAY-25/US_STATES-GAME/States_to_learn.csv")
+        break
+    
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t= turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        ans_row=data[data["state"] == answer_state]
+        t.goto(int(ans_row.x),int(ans_row.y))
+        t.write(answer_state)
+
+
+
+
+
