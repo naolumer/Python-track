3fa3a9cc21de4af3adfd36d294520395f1466aaa
naolumer <naolumer3@gmail.com>
2024-09-12 09:17:36 +0300

    valid parentheses

---
+++ Leetcode Easy/valid-parenthesis.py
@@ -0,0 +1,15 @@
+
+def isValid(self, s):
+
+        brackets={")":"(","]":"[","}":"{"}
+        stack=[]
+        
+        for c in s:
+            if c in brackets:
+                if stack and stack[-1]==brackets[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return len(stack)==0
