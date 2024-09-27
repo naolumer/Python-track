fb73731912566fb821a1279e4782cfdae76fd8dc
naolumer <naolumer3@gmail.com>
2024-09-27 18:44:32 +0300

    equivalent string arrays

---
+++ Leetcode Easy/equivalent string arrays.py
@@ -0,0 +1,10 @@
+def arrayStringsAreEqual(word1, word2):
+        
+        wordd1= ""
+        for stringg in word1:
+            wordd1+= stringg
+        wordd2 = ""
+        for stringg in word2:
+            wordd2 += stringg
+
+        return wordd1 == wordd2
