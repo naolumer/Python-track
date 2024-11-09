440dee9b7e6db47920f2b4d84a04fc9f55064143
naolumer <naolumer3@gmail.com>
2024-11-09 17:42:33 +0300

    reverse linked list

---
+++ Leetcode Easy/reverse-linked-list.py
@@ -0,0 +1,13 @@
+def reverseList(head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        prev,curr = None,head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev= curr
+            curr = temp
+        return prev
