7a13e1fe59f869b0e1bbc870613ff1b993a1df03
naolumer <naolumer3@gmail.com>
2024-12-16 15:44:24 +0300

    numberOfStepsToReduce-a-NumberToZero

---
+++ Leetcode Easy/number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,10 @@
+def numberOfSteps(self, num):
+    counter = 0
+    while num > 0:
+        if num%2==0:
+            num=num/2
+            counter+=1
+        else:
+            num=num-1
+            counter+=1
+    return counter
