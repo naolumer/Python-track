5998ab96f8ac023ff0762e1f5e97f2db35010aae
naolumer <naolumer3@gmail.com>
2024-12-17 21:00:36 +0300

    Better solution for PlusOne problem

--- Leetcode Easy/plusone.py
+++ Leetcode Easy/plusone.py
@@ -1,19 +1,7 @@
-
 def plusOne(digits):
-        
-    strdigits = map(str, digits)
-    stringg =""
-    for num in strdigits:
-        stringg+=num
-    
-    numstring = int(stringg)
-    plusone= numstring +1
-    backtostring = str(plusone)
+    plusone = int("".join([str(d) for d in digits])) + 1
+    return [int(d) for d in str(plusone)]
 
-    finalist = []
-    for char in backtostring:
-        finalist.append(char)
-    
-    result = map(int, finalist)
 
-    return result
+    
+    
