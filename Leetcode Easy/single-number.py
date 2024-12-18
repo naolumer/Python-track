82ae6dd002270a7772b1113d499903e4eabe8ac5
naolumer <naolumer3@gmail.com>
2024-12-18 14:44:17 +0300

    XOR operator solution for single-number problem

--- Leetcode Easy/single-number.py
+++ Leetcode Easy/single-number.py
@@ -1,19 +1,38 @@
-class Solution(object):
-    def singleNumber(self, nums):
+from collections import Counter
+def singleNumber( nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        dicc= {}
+        # dicc= {}
         
-        counter= len(nums)
-        for num in nums:
-            if num in dicc:
-                dicc[num]+=1
-            else:
-                dicc[num]=1
+        # counter= len(nums)
+        # for num in nums:
+        #     if num in dicc:
+        #         dicc[num]+=1
+        #     else:
+        #         dicc[num]=1
                     
-        for keys in dicc:
-            if dicc[keys]!=2:
-                n= keys
-        return n
+        # for keys in dicc:
+        #     if dicc[keys]!=2:
+        #         n= keys
+        # return n
+
+        # Using hashmaps
+
+        count = Counter(nums)
+        for key,count in count.items():
+              if count == 1:
+                    return key
+        
+        
+        
+        
+        
+        # More efficient solution using XOR
+
+
+        unique = 0
+        for num in nums:
+            unique^=num      # XOR each number with the current unique value
+        return unique
