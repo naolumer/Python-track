07a7914a0d6077eef4b1e2437e8cde9a2e1eaa1f
naolumer <naolumer3@gmail.com>
2024-12-18 23:07:45 +0300

    better solution for majority element

--- Leetcode Easy/majority-element.py
+++ Leetcode Easy/majority-element.py
@@ -1,18 +1,30 @@
+from collections import Counter
 def majorityElement(nums):
-    n= len(nums)
-    majority = n/2
+    # n= len(nums)
+    # majority = n/2
 
-    appearanceDic={}
+    # appearanceDic={}
 
-    for num in nums:
-        if num in appearanceDic:
-            appearanceDic[num]+=1
-        else:
-            appearanceDic[num]=1
+    # for num in nums:
+    #     if num in appearanceDic:
+    #         appearanceDic[num]+=1
+    #     else:
+    #         appearanceDic[num]=1
     
-    for num in appearanceDic:
-        if appearanceDic[num] > majority:
-            return num
+    # for num in appearanceDic:
+    #     if appearanceDic[num] > majority:
+    #         return num
 
 # test case 👇
-# print(majorityElement([3,2,3,2,2]))   
+# print(majorityElement([3,2,3,2,2]))  
+
+    # BETTER SOLUTION;
+
+    count = Counter(nums)
+    n = len(nums)
+
+    for key,freq in count.items():
+        if freq > n/2:
+            return key
+
+
