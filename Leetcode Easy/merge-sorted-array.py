1d288c855a81bb275f3efd43fb17865eb93a41b2
naolumer <naolumer3@gmail.com>
2024-12-18 13:25:11 +0300

    merge-sorted-array

---
+++ Leetcode Easy/merge-sorted-array.py
@@ -0,0 +1,19 @@
+def merge(nums1,nums2,m,n)-> None: 
+    # This function will not return anything, it sorts the array(nums1) in-place
+    last = m+n-1
+
+    while m>0 and n>0:
+        if nums1[m-1] > nums2[n-1]:
+            nums1[last] = nums1[m-1]
+            m-=1
+        else:
+            nums1[last] = nums2[n-1]
+            n-=1
+        last-=1
+    while n>0:
+        nums1[last] = nums2[n-1]
+        n-=1
+        last-=1
+    
+        
+
