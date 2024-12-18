c662a468a01e760c20d707a049db72424ecb7b61
naolumer <naolumer3@gmail.com>
2024-12-18 23:28:36 +0300

    contains-duplicate leetcode

---
+++ Leetcode Easy/contains-duplicate.py
@@ -0,0 +1,8 @@
+from collections import Counter
+def containsDuplicate(nums):
+    count = Counter(nums)
+
+    for key,freq in count.items():
+        if freq >= 2:
+            return True
+    return False~
