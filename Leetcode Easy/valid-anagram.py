6c1d6f7511cba1805666ee96a7af6336aaa20acc
naolumer <naolumer3@gmail.com>
2024-12-19 11:14:02 +0300

    shorter and efficient solution for valid anagram

--- Leetcode Easy/valid-anagram.py
+++ Leetcode Easy/valid-anagram.py
@@ -1,16 +1,31 @@
-
+from collections import Counter
 def isAnagram(s, t):
       
-        freq_s= {}
-        freq_t={}
-        for char in s:
-            if char in freq_s:
-                freq_s[char]+=1
-            else:
-                freq_s[char]=1
-        for char in t:
-            if char in freq_t:
-                freq_t[char]+=1
-            else:
-                freq_t[char]=1
-        return freq_t==freq_s
+        # freq_s= {}
+        # freq_t={}
+        # for char in s:
+        #     if char in freq_s:
+        #         freq_s[char]+=1
+        #     else:
+        #         freq_s[char]=1
+        # for char in t:
+        #     if char in freq_t:
+        #         freq_t[char]+=1
+        #     else:
+        #         freq_t[char]=1
+        # return freq_t==freq_s
+
+        # BETTER SOLUTION
+
+        count_t = Counter(t)
+        count_s = Counter(s)
+
+        if len(s)!=len(t):
+                return False
+
+        for key,frq in count_t.items():
+                if not key in count_s or not count_s[key] == frq:
+                        return False
+        return True
+
+print(isAnagram("car","rat"))
